0bc20dd00bea9859c05400321624dface5c70e8b
jamcry <jamcry@hotmail.com>
2019-08-02 13:40:41 +0300

    Complete new message form

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 const messages = [
   {
+    text: 'So long, and thanks for all the fish!',
+    user: 'D. N. A.',
+    added: new Date('May 11, 2001 12:00:00'),
+  },
+  {
     text: 'Hi there!',
     user: 'Amando',
     added: new Date()
@@ -10,16 +15,35 @@ const messages = [
     text: 'Hello, world!',
     user: 'Cem',
     added: new Date()
-  }
+  },
+  
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', messages: messages });
+  // Pass reversed messages to show in descending date order
+  res.render('index', { title: 'Express Message Board', messages: messages.reverse() });
 });
 
 /* GET new message page. */
 router.get('/new', function(req, res, next) {
-  res.render('form', { title: 'New message' });
-})
+  res.render('form', { title: 'New Message' });
+});
+
+/* POST new message page */
+router.post('/new', function(req, res, next) {
+  // Get username and message from request body
+  let { username, message } = req.body;
+
+  // Push new message to messages array
+  messages.push({
+    text: message,
+    user: username,
+    added: new Date(),
+  });
+
+  // Redirect to homepage (main message board)
+  res.redirect('/');
+});
+
 module.exports = router;
